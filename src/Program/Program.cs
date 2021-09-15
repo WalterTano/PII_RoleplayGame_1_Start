64147d6f19cd5786406556f58d748151a4bee0ee
Ezequiel Gomez <ezecam2019@gmail.com>
2021-09-15 19:51:48 -0300

    agrego personajes y dos items

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using Library;
 
 namespace Program
 {
@@ -6,6 +8,17 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            Raza guerrero = new Raza("Guerrero");
+            Personaje guerreroConEspada =  new Personaje("Gran Guerrero", guerrero, 10, 15, 5);
+            
+            ArrayList lista1= new ArrayList();
+            lista1.Add(guerrero);
+            Item espadaSagrada = new Item("Espada Sagrada", lista1, 12, 14, null);
+            Item espada = new Item("Espada normal", lista1, 2, 14, null);
+            
+            guerreroConEspada.AgregarItem(espadaSagrada);
+            guerreroConEspada.AgregarItem(espada);
+            
         }
     }
 }
