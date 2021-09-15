454a5f8d5aced83afc88c34fd25962165cb0d2bf
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-09-15 20:04:06 -0300

    agrego personaje

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,6 +6,10 @@ namespace Program
     {
         static void Main(string[] args)
         {
+            Personaje Bob= new Personaje("Bob","Elfo",80,25,48);
+            Item Boliadora= new Item("Arma",["Elfo","Mago"],48,0,[]);
+            Item Escudo= new Item("Defensa",["Elfo","Mago","Enano"],0,70,[]);
+
         }
     }
 }
