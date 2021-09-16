61ec5796b2cb795be8466e36378229dbc106ec75
Walter Taño <tano.walter@hotmail.com>
2021-09-15 23:58:07 -0300

    Agrego personajes y dos items (tres con el hechizo)

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,11 +1,28 @@
 ﻿using System;
+using Library;
+using System.Collections;
 
 namespace Program
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            Raza MAGO = new Raza("Mago");
+            Personaje personajeWalter = new Personaje("Walter", MAGO, 100, 35, 35);
+
+            Item hechizoWalter = new Item("Hechizo", new ArrayList(), 10, 0, new ArrayList());
+            ArrayList razasMago = new ArrayList();
+            razasMago.Add(MAGO);
+            Item bastonWalter = new Item("Ofensivo", razasMago, 20, 0, new ArrayList());
+            bastonWalter.AgregarHechizo(hechizoWalter);
+            Item tunica = new Item("Defensivo", razasMago, 0, 5, new ArrayList());
+
+            personajeWalter.AgregarItem(bastonWalter);
+            personajeWalter.AgregarItem(tunica);
         }
+
+        
     }
 }
