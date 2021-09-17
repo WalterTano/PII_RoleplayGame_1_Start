7b004dc6727e2c7a8f6da1d179f573a0ece16362
[Faustino <[faustino.harriague@correo.ucu.edu.uy]>
2021-09-17 19:47:16 -0300

    Personajes e itemas_Faustino

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,9 +6,16 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Personaje Bob= new Personaje("Bob","Elfo",80,25,48);
-            Item Boliadora= new Item("Arma",["Elfo","Mago"],48,0,[]);
-            Item Escudo= new Item("Defensa",["Elfo","Mago","Enano"],0,70,[]);
+            //Faustino, Personajes e Items//
+            Raza Bruto= new Raza("bruto");
+            Personaje Bob= new Personaje("Bob",Bruto,19,44,22);
+            
+            Item Palo= new Item("palo",Bruto,48,0,23);
+            Item Navaja= new Item("navaja",Bruto,10,0,15);
+
+            Bob.AgregarItem(Palo);
+            Bob.AgregarItem(Navaja);
+            
 
         }
     }
