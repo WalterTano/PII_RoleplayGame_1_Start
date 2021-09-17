7699622e4124cabeee40e56f09e1392e7c3b2881
Lucas Giffuni <lucasgiffuni@gmail.com>
2021-09-17 11:09:59 -0300

    Test cases added

--- src/Test/Library.Test/ExampleTest.cs
+++ src/Test/Library.Test/ExampleTest.cs
@@ -1,5 +1,5 @@
 using NUnit.Framework;
-
+using Library;
 namespace Test.Library
 {
 
@@ -11,6 +11,52 @@ namespace Test.Library
         public void dummyTest()
         {
             Assert.True(true);
+        }
+
+
+        // Lucas Giffuni
+        [Test]
+        public void TestRaza()
+        {
+            Raza r = new Raza("Ogro");
+            string expected = "Ogro";
+            Assert.AreEqual(expected, r.Nombre);
+        }
+        
+        [Test]
+        public void TestName()
+        {
+            Raza ogro = new Raza("Ogro");
+            Personaje p = new Personaje("Uglúk", ogro, 1000, 200, 125);
+            string expected = "Uglúk";
+            Assert.AreEqual(expected, p.Nombre);
+        }
+
+        [Test]
+        public void TestVida()
+        {
+            Raza ogro = new Raza("Ogro");
+            Personaje p = new Personaje("Uglúk", ogro, 1000, 200, 125);
+            int expected = 1000;
+            Assert.AreEqual(expected, p.Vida);
+        }
+
+        [Test]
+        public void TestAtaque()
+        {
+            Raza ogro = new Raza("Ogro");
+            Personaje p = new Personaje("Uglúk", ogro, 1000, 200, 125);
+            int expected = 200;
+            Assert.AreEqual(expected, p.Ataque);
+        }
+
+        [Test]
+        public void TestDefensa()
+        {
+            Raza ogro = new Raza("Ogro");
+            Personaje p = new Personaje("Uglúk", ogro, 1000, 200, 125);
+            int expected = 125;
+            Assert.AreEqual(expected, p.Defensa);
         }
 
     }
