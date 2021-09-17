fcf005f6f94da31708236e30e19a9c66ceb45501
Ezequiel Gomez <ezecam2019@gmail.com>
2021-09-17 19:03:28 -0300

    fix

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -51,10 +51,10 @@ namespace Program
             Raza guerrero = new Raza("Guerrero");
             Personaje guerreroConEspada =  new Personaje("Gran Guerrero", guerrero, 10, 15, 5);
             
-            ArrayList lista1= new ArrayList();
+            ArrayList lista1 = new ArrayList();
             lista1.Add(guerrero);
-            Item espadaSagrada = new Item("Espada Sagrada", lista1, 12, 14, null);
-            Item espada = new Item("Espada normal", lista1, 2, 14, null);
+            Item espadaSagrada = new Item("Espada Sagrada", lista1, 12, 14);
+            Item espada1 = new Item("Espada normal", lista1, 2, 14);
             
             guerreroConEspada.AgregarItem(espadaSagrada);
             guerreroConEspada.AgregarItem(espada);
